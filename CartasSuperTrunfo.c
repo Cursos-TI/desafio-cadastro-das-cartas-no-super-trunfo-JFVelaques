855660d7c010c493a278296ba9eb0388a1a8ff3c
João Felipe Velasques <joaovelasques2003@gmail.com>
2025-03-20 13:15:35 +0000

    Desafio super trunfo novato finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 //Teste joao felipe
 
 int main() {
-    printf("Desafio Super Trunfo Novato!\n")
+    printf("Desafio Super Trunfo Novato!\n");
     char estado1 [50], estado2 [50], codigo1 [50], codigo2 [50], cidade1 [50], cidade2 [50];
     int populacao1, populacao2;
     float area1, area2, pib1, pib2;
@@ -33,6 +33,28 @@ int main() {
 
     printf("Digite a quantidade de Pontos Turísticos: \n");
     scanf("%d, &pontosturisticos1");
+
+    printf("Digite seu estado: \n");
+    scanf("%s, estado2");
+
+    printf("Digite o código: \n");
+    scanf("%s, codigo2");
+
+    printf("Digite a cidade: \n");
+    scanf("%s, cidade2");
+
+    printf("Digite a população: \n");
+    scanf("%d, &populacao2");
+
+    printf("Digite a área: \n");
+    scanf("%f, &area2");
+
+    printf("Digite o PIB: \n");
+    scanf("%f, &pib2");
+
+    printf("Digite a quantidade de Pontos Turísticos: \n");
+    scanf("%d, &pontosturisticos2");
+
 
     
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
